02a1c96e23f84e81cb91084e865cf976928cfd99
Karine Bialobroda <karine.bialobroda@gmail.com>
2016-08-01 17:22:06 +0200

    new html/css

--- config.rb
+++ config.rb
@@ -31,6 +31,7 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+
 ###
 # Helpers
 ###
